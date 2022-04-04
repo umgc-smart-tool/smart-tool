af1a74bc0b2fc72cfacc4321a82a850e40834208
samuel.b.scalf <sscalf2@student.umgc.edu>
2022-04-03 21:46:30 -0700

    create way to access data from UI
    While working on this issue, I realized it would be better for the view to just send commands directly to its `DataAccessor` instead of the App.  The App is used to set up the environment, not necessarily micro-manage everything.  In doing so, I also realized that the views may be better off being extensions of an abstract class (GUI is a View; Console is a View).

--- src/edu/umgc/smart/controller/App.java
+++ src/edu/umgc/smart/controller/App.java
@@ -5,11 +5,10 @@ import edu.umgc.smart.view.View;
 
 public class App {
   private View view;
-  private DataAccessor dataAccessor;
 
   public App(View view, DataAccessor dataAccessor) {
     this.view = view;
-    this.dataAccessor = dataAccessor;
+    view.setDataAccessor(dataAccessor);
   }
 
   public void start() {
