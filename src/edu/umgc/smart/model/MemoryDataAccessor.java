90d09bce840fd678a28b7765ef3b9606cfffcc1b
samuel.b.scalf <sscalf2@student.umgc.edu>
2022-04-03 22:21:42 -0700

    create single test record
    - created a single test record in MemoryDataAccessor and created a way to get a string value of a Record.
    - modified ConsoleView to print all records then pause.
      - I will most likely be using ConsoleView for testing until the GUI is completed.

--- src/edu/umgc/smart/model/MemoryDataAccessor.java
+++ src/edu/umgc/smart/model/MemoryDataAccessor.java
@@ -1,10 +1,25 @@
 package edu.umgc.smart.model;
 
+import java.sql.Date;
 import java.util.ArrayList;
 import java.util.List;
 
 public class MemoryDataAccessor implements DataAccessor {
   private List<Record> records = new ArrayList<>();
+
+  public MemoryDataAccessor() {
+    Record record = new Record();
+    record.setReferenceNumber("DOC-R-21-01234");
+    record.setTitle("Finance Report for FY21 Q3");
+    record.setDocumentType("Record");
+    record.setAuthorLastName("Lastname");
+    record.setAuthorFirstName("Firstname");
+    record.setDate(Date.valueOf("2021-10-15"));
+    record.setCategory("Finance");
+    record.setSummary("This report covers the finances for the 3rd quarter of 2021.");
+    record.setLocation("/network/folder/reports/Q3");
+    records.add(record);
+  }
 
   @Override
   public Record get(int id) {
