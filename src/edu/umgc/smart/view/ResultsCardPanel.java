0801ed5f5d9082399ee8133d26ef076b5f6f271e
samuel.b.scalf <sscalf2@student.umgc.edu>
2022-04-30 15:11:39 -0700

    refactor ResultsCardPanel again
    First time refactoring modified behavior in an undesirable way

--- src/edu/umgc/smart/view/ResultsCardPanel.java
+++ src/edu/umgc/smart/view/ResultsCardPanel.java
@@ -1,22 +1,22 @@
 package edu.umgc.smart.view;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.GridLayout;
-import java.awt.BorderLayout;
 import java.awt.Insets;
 import java.awt.event.ActionListener;
 import java.util.logging.Logger;
 
+import javax.swing.BorderFactory;
 import javax.swing.JButton;
-import javax.swing.JPanel;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JTextField;
 import javax.swing.SwingConstants;
-import javax.swing.JScrollPane;
-import javax.swing.BorderFactory;
-import javax.swing.JOptionPane;
 
 import edu.umgc.smart.model.Record;
 
@@ -25,48 +25,44 @@ public class ResultsCardPanel extends CardPanel {
     private static Record[] records;
     private static String searchTerm;
     private static String searchTermField;
+
+    private JLabel searchLabel = new JLabel("General Search: ");
     private JTextField searchField = new JTextField();
     private JButton searchButton = new JButton("Search");
     private JButton advancedSearchButton = new JButton("Advanced Search");
-    private JLabel searchLabel = new JLabel("General Search: ");
-    private CardView cardView;
+
     private JButton[] selectButtons;
 
     public ResultsCardPanel(CardView cardView) {
-        this(cardView, null, null, null);
-    }// End ResultsCardPanel(CardView cardView) constructor
+        this(cardView, new Record[0], "", "");
+    }
 
     public ResultsCardPanel(CardView cardView, Record[] recordsArray, String searchTermItem, String searchFieldItem) {
-        this.cardView = cardView;
         ResultsCardPanel.setSearchTerm(searchTermItem);
         ResultsCardPanel.setRecordsArray(recordsArray);
         ResultsCardPanel.setSearchFieldItem(searchFieldItem);
-        initializePanel();
-    }
-
-    private void initializePanel() {
         this.setLayout(new BorderLayout());
-        addSearchPanel(BorderLayout.NORTH);
-        addResultsPanel(BorderLayout.CENTER);
+        initializeSearchPanel(cardView);
+        initializeResultsPanel();
     }
 
-    private void addSearchPanel(String location) {
+    private void initializeSearchPanel(CardView cardView) {
         JPanel searchPanel = new JPanel();
         searchPanel.setLayout(new GridBagLayout());
-        initializeSelectButtons();
-        addActionListeners();
+        initializeSelectButtonsWithLoop();
+        addActionListeners(cardView);
         addComponentsToSearchPanel(searchPanel);
-        this.add(searchPanel, location);
+        this.add(searchPanel, BorderLayout.NORTH);
     }
 
-    private void initializeSelectButtons() {
+    private void initializeSelectButtonsWithLoop() {
         selectButtons = new JButton[records.length];
         for (int i = 0; i < selectButtons.length; i++) {
             selectButtons[i] = new JButton("Select");
         }
     }
 
-    private void addActionListeners() {
+    private void addActionListeners(CardView cardView) {
         ActionListener searchAction = e -> {
             LOGGER.info("Search button pressed: " + searchField.getText());
             if (searchField.getText().isEmpty()) {
@@ -78,13 +74,14 @@ public class ResultsCardPanel extends CardPanel {
                         "All Fields"));
             }
         };
+
         searchButton.addActionListener(searchAction);
         searchField.addActionListener(searchAction);
         advancedSearchButton.addActionListener(e -> cardView.setPanel(new AdvancedSearchCardPanel(cardView)));
-        addSelectButtonListenersWithLoop();
+        addSelectButtonListenersWithLoop(cardView);
     }
 
-    private void addSelectButtonListenersWithLoop() {
+    private void addSelectButtonListenersWithLoop(CardView cardView) {
         for (int i = 0; i < selectButtons.length; i++) {
             final int finalI = i;
             selectButtons[i]
@@ -119,33 +116,30 @@ public class ResultsCardPanel extends CardPanel {
         searchPanel.add(advancedSearchButton, constraints);
     }
 
-    private void addResultsPanel(String location) {
+    private void initializeResultsPanel() {
         JPanel resultsPanel = new JPanel();
         resultsPanel.setLayout(new BorderLayout());
         JScrollPane resultScrollPane = new JScrollPane(resultsPanel);
-
         resultsPanel.setLayout(new GridLayout(records.length, 1));
-        resultsPanel.setBorder(
-                BorderFactory.createTitledBorder(
-                        "Results for: \"" + searchTerm + "\" in field: " + searchTermField));
-        addResultsToPanel(resultsPanel);
-        this.add(resultScrollPane, location);
+        resultsPanel.setBorder(BorderFactory.createTitledBorder("Results for: \"" + searchTerm + "\" in field: " +
+                searchTermField));
+        addResultsToResultsPanel(resultsPanel);
+        this.add(resultScrollPane, BorderLayout.CENTER);
     }
 
-    private void addResultsToPanel(JPanel resultsPanel) {
-        if (records.length > 0) {
-            addResultsToPanelWithLoop(resultsPanel);
-        } else {
+    private void addResultsToResultsPanel(JPanel resultsPanel) {
+        if (records.length == 0) {
             JLabel noResultLabel = new JLabel("There are no results for: \"" + searchTerm + "\" in the field: " +
                     searchTermField, SwingConstants.CENTER);
             resultsPanel.add(noResultLabel);
+        } else {
+            addResultsWithLoop(resultsPanel);
         }
     }
 
-    private void addResultsToPanelWithLoop(JPanel resultsPanel) {
+    private void addResultsWithLoop(JPanel resultsPanel) {
         for (int i = 0; i < records.length; i++) {
             JPanel recordPanel = new JPanel();
-            recordPanel.setSize(750, 500);
             recordPanel.setLayout(new GridLayout(1, 4));
             recordPanel.setBorder(BorderFactory.createLineBorder(Color.BLACK));
             selectButtons[i].setBorderPainted(false);
@@ -153,7 +147,7 @@ public class ResultsCardPanel extends CardPanel {
             recordPanel.add(selectButtons[i]);
             recordPanel.add(new JLabel(records[i].getReferenceNumber()));
             recordPanel.add(new JLabel(records[i].getTitle()));
-            recordPanel.add(new JLabel(records[i].toString()));
+            recordPanel.add(new JLabel(records[i].getDate().toString()));
             resultsPanel.add(recordPanel);
         }
     }
