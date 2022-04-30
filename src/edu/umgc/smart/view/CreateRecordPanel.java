69aa739827377e229f7334e49adafd3fef296bbc
samuel.b.scalf <sscalf2@student.umgc.edu>
2022-04-30 15:21:40 -0700

    update and refactor CreateRecordPanel
    Saving now "works"

--- src/edu/umgc/smart/view/CreateRecordPanel.java
+++ src/edu/umgc/smart/view/CreateRecordPanel.java
@@ -5,26 +5,40 @@ import java.util.logging.Logger;
 import javax.swing.JButton;
 import javax.swing.JOptionPane;
 
+import edu.umgc.smart.model.Record;
+
 public class CreateRecordPanel extends RecordCardPanel {
     private static final Logger LOGGER = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
 
     CreateRecordPanel(CardView cardView) {
         super(cardView);
         enableReturnButton(false);
-        // TODO: Implement saving a new record to the data set
         setSaveButton(
-                new JButton("Save New Record"),
-                e -> {
-                    int saveOption = JOptionPane.showConfirmDialog(null,
-                            "Do you want to save this record?");
-                    if (saveOption == 0) { // If yes to saving record
-                        LOGGER.info("Save - new record");
-                        JOptionPane.showMessageDialog(null, "Creating new records through the GUI\n" +
-                                "is not yet implemented.");
-                    } else { // Else answer is no to save, or cancelled
-                        LOGGER.info("Save cancelled.");
-                    }
-                });
+                new JButton("Save Button Pressed"),
+                e -> saveRecord(cardView));
+    }
+
+    private void saveRecord(CardView cardView) {
+        int saveOption = JOptionPane.showConfirmDialog(null,
+                "Do you want to save this record?");
+        if (saveOption == JOptionPane.YES_OPTION) {
+            saveNewRecord(cardView);
+        } else {
+            LOGGER.info("Save cancelled.");
+        }
+    }
+
+    private void saveNewRecord(CardView cardView) {
+        LOGGER.info("Save - new record");
+        Record newRecord = buildRecord();
+        if (null != newRecord) {
+            cardView.dataAccessor.save(newRecord);
+        } else {
+            JOptionPane.showMessageDialog(this,
+                    "Reference Number is not valid",
+                    "Invalid Record",
+                    JOptionPane.ERROR_MESSAGE);
+        }
     }
 
     @Override
