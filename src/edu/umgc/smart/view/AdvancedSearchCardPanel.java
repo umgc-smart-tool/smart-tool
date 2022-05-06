4e9b80a4f6d5dcbe071768d0db79ea1a50e99ecc
samuel.b.scalf <sscalf2@student.umgc.edu>
2022-05-06 16:35:28 -0700

    remove unused imports

--- src/edu/umgc/smart/view/AdvancedSearchCardPanel.java
+++ src/edu/umgc/smart/view/AdvancedSearchCardPanel.java
@@ -5,8 +5,6 @@ import java.awt.Color;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.util.Arrays;
 import java.util.Locale;
 import java.util.logging.Level;
